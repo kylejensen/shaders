c1525863bae3504b027f65f26d8e548981a25e04
Kyle Jensen <kyle.jensen@bynd.com>
2017-08-27 10:53:46 -0600

    Adds wormhole shader

--- README.md
+++ README.md
@@ -4,3 +4,5 @@ Various shaders written in GLSL
 
 ##Shadertoy
 1. [Black and White Shader](shadertoy/blackandwhite.glsl)
+2. [Wormhole Shader](shadertoy/wormhole.glsl)
+<img src="img/wormhole.gif" />
---
+++ shadertoy/wormhole.glsl
@@ -0,0 +1,24 @@
+void mainImage( out vec4 fragColor, in vec2 fragCoord )
+{
+	// normalized coordinates (-1 to 1 vertically)
+    vec2 p = (-iResolution.xy + 2.0*fragCoord)/iResolution.y;
+
+    // angle of each pixel to the center of the screen
+    float a = atan(p.y,p.x);
+
+    // cylindrical tunnel
+    float r = length(p);
+    
+    // index texture by (animated inverse) radius and angle
+    vec2 uv = vec2( 0.3/r + 0.2*iTime, a/3.1415927 );
+
+    vec2 uv2 = vec2( uv.x, atan(p.y,abs(p.x))/3.1415927 );
+    
+    // Get texture in iChannel0
+    vec3 col = textureGrad( iChannel0, uv, dFdx(uv2), dFdy(uv2) ).xyz;
+    
+    // darken at the center    
+    col = col*r;
+    
+    fragColor = vec4( col, 1.0 );
+}
